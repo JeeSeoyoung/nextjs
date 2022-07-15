b2e2cc8294ea4204c7572892493d99b8edda6b5f
Holllywaterr <holywater@handong.ac.kr>
2022-07-15 15:57:31 +0900

    자기소개

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
-import Head from 'next/head'
-import utilStyles from '../styles/utils.module.css'
- 
-
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>Sungsu</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>자기소개</p>
-        <p>제 이름은 ~~</p>
-      </section>
-    </Layout>
-  )
+    <h1>Hello, I am Sungsu</h1>
+  );
 }
